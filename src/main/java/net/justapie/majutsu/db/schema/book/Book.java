ddd6c7b9764fee49f1960be386e0b58aaf64a48c
Do Tran Thai An <justapie06@gmail.com>
2025-07-16 21:55:01 +0700

    fix: invalid types

--- src/main/java/net/justapie/majutsu/db/schema/book/Book.java
+++ src/main/java/net/justapie/majutsu/db/schema/book/Book.java
@@ -1,5 +1,8 @@
 package net.justapie.majutsu.db.schema.book;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public class Book extends Document {
     private String type;
     private String language;
@@ -18,7 +21,7 @@ public class Book extends Document {
         this.publisher = publisher;
         this.isbn = isbn;
         this.createdAt = LocalDate.now();
-        this.status = DocumentEnum.AVAILABLE;
+        this.status = DocumentStatus.AVAILABLE;
         this.type = type;
         this.language = language;
         this.pageCount = pageCount;
@@ -34,7 +37,7 @@ public class Book extends Document {
         this.publisher = publisher;
         this.isbn = isbn;
         this.createdAt = LocalDate.now();
-        this.status = DocumentEnum.AVAILABLE;
+        this.status = DocumentStatus.AVAILABLE;
         this.type = type;
         this.language = language;
         this.pageCount = pageCount;
@@ -75,7 +78,7 @@ public class Book extends Document {
     @Override
     public void borrow(long borrowerId, int loanDays) {
         if (isAvailableForLoan()) {
-            this.status = DocumentEnum.BORROWED;
+            this.status = DocumentStatus.BORROWED;
             this.borrowerId = borrowerId;
             this.borrowDate = LocalDate.now();
             this.dueDate = this.borrowDate.plusDays(loanDays);
