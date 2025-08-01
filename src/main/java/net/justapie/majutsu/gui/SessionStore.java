a52486cea6d060bc63bd6cc0ac37f07b60395c64
Do Tran Thai An <justapie06@gmail.com>
2025-08-01 18:59:43 +0700

    refactor: simplify DocumentRepository and implement caching

--- src/main/java/net/justapie/majutsu/gui/SessionStore.java
+++ src/main/java/net/justapie/majutsu/gui/SessionStore.java
@@ -1,7 +1,11 @@
 package net.justapie.majutsu.gui;
 
+import net.justapie.majutsu.cache.Cache;
+import net.justapie.majutsu.cache.CacheObject;
 import net.justapie.majutsu.db.repository.user.UserRepositoryFactory;
 import net.justapie.majutsu.db.schema.user.User;
+
+import java.util.Objects;
 
 public class SessionStore {
     private static final SessionStore INSTANCE = new SessionStore();
@@ -20,7 +24,12 @@ public class SessionStore {
     }
 
     public User getCurrentUser() {
-        return UserRepositoryFactory.getInstance().create().getUserById(this.currentUserId);
+        CacheObject<User> user = Cache.getInstance().get("user");
+        if (Objects.isNull(user)) {
+            return UserRepositoryFactory.getInstance().create().getUserById(this.currentUserId);
+        }
+
+        return user.getData();
     }
 
     public void clearSession() {
