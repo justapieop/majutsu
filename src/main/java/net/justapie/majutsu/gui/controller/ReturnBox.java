5be9dcf39063ffaad26396f10c11df8dffa2d92b
NahIdWinnnn <24021406@vnu.edu.vn>
2025-08-05 20:17:08 +0700

    sync: from borrow to return

--- src/main/java/net/justapie/majutsu/gui/controller/ReturnBox.java
+++ src/main/java/net/justapie/majutsu/gui/controller/ReturnBox.java
@@ -1,32 +1,44 @@
 package net.justapie.majutsu.gui.controller;
 
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.stage.Stage;
+import javafx.geometry.Pos;
+import javafx.scene.control.CheckBox;
+import net.justapie.majutsu.db.schema.book.Book;
 
-public class ReturnBox implements Selectable {
-    private static final BorrowBox INSTANCE = new BorrowBox();
-    private Stage currentStage;
+import java.util.ArrayList;
+import java.util.List;
 
-    public static BorrowBox getInstance() {
-        return INSTANCE;
-    }
-
-    public void setStage(Stage stage) {
-        this.currentStage = stage;
-    }
-
-    public void terminateWindow() {
-        currentStage.close();
-        currentStage = null;
-    }
-
-    public ReturnBox() {
-    }
-
-    @FXML
-    private void setOnConfirmButton(ActionEvent event) {
-
-        getInstance().terminateWindow();
+public class ReturnBox extends BookModificationBase {
+    @Override
+    public void setSelectionSection(List<Book> source) {
+        selectedOptions = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            CheckBox box = new CheckBox(String.format("%s: %s", i, i));
+            box.setAlignment(Pos.CENTER_LEFT);
+            final int index = i;
+            box.setOnAction(e -> {
+                if (box.isSelected()) {
+                    System.out.println("Siuuuuuuu" + index);
+                }
+                else {
+                    System.out.println("Undo siuuuuu" + index);
+                }
+            });
+            selectionSection.getChildren().add(box);
+        }
+        for (int i = 0; i < source.size(); i++) {
+            final Book book = source.get(i);
+            CheckBox box = new CheckBox(String.format("%s: %s.", book.getId(), book.getVolumeInfo().getTitle()));
+            box.setAlignment(Pos.CENTER_LEFT);
+            final int index = i;
+            box.setOnAction(e -> {
+                if (box.isSelected()) {
+                    selectedOptions.add(index);
+                }
+                else {
+                    selectedOptions.remove(index);
+                }
+            });
+            selectionSection.getChildren().add(box);
+        }
     }
 }
