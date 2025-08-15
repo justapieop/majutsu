c005e1b95e2a5733bfedf19f864ceed73dc9c521
Do Tran Thai An <justapie06@gmail.com>
2025-08-15 14:48:45 +0700

    chore: add debug log

--- src/main/java/net/justapie/majutsu/gui/controller/BaseController.java
+++ src/main/java/net/justapie/majutsu/gui/controller/BaseController.java
@@ -1,19 +1,17 @@
 package net.justapie.majutsu.gui.controller;
 
-import javafx.scene.Node;
+import ch.qos.logback.classic.Logger;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import net.justapie.majutsu.gui.SceneManager;
 import net.justapie.majutsu.gui.UI;
+import net.justapie.majutsu.utils.Utils;
 
 public abstract class BaseController {
-    public void closeScene(Node node) {
-        Stage stage = (Stage) node.getScene().getWindow();
-
-        stage.close();
-    }
+    private static final Logger LOGGER = Utils.getInstance().getRootLogger();
 
     public void switchToScene(String path) {
+        LOGGER.debug("Loading scene {}", path);
         Stage stage = UI.stage;
 
         Scene newScene = SceneManager.loadScene(path);
